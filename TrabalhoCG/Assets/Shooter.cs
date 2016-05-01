6bdcfe0febdd124b0281166cbbc4570d6f7b4c8e
Rodrigo Werneck <rodrigowfranco@gmail.com>
2016-05-01 18:40:22 -0300

    Primeira versão do jogo, só com o funcionamento básico

---
+++ TrabalhoCG/Assets/Shooter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public GameObject bulletRef;
+    public float bulletSpeed;
+    public KeyCode shootKey;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(shootKey))
+        {
+            GameObject instance = Instantiate(bullet, bulletRef.transform.position, bulletRef.transform.rotation) as GameObject;
+            Rigidbody bulletRigidbody = instance.GetComponent<Rigidbody>();
+            if (bulletRigidbody != null)
+            {
+                bulletRigidbody.AddForce(instance.transform.forward * bulletSpeed);
+            }
+        }
+
+
+    }
+}
